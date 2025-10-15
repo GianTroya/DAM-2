e017d76476e2d0a901a92c344e8a65853ed221c1
GianTroya <giancarlorojastroya@gmail.com>
2025-10-15 19:31:24 +0200

    Organización Caso-Práctico-1.1 de Acceso a Datos

--- Acceso-A-Datos/01-Introduccion-A-Ficheros/Caso-practico-1.1/src/Ejemplos/GestionSimpleFichero.java
+++ Acceso-A-Datos/01-Introduccion-A-Ficheros/Caso-practico-1.1/src/Ejemplos/GestionSimpleFichero.java
@@ -1,3 +1,5 @@
+package Ejemplos;
+
 import java.io.File;
 import java.io.IOException;
 
---
+++ Acceso-A-Datos/01-Introduccion-A-Ficheros/Caso-practico-1.1/src/Ejercicios/GuardarArchivos.java
@@ -0,0 +1,10 @@
+package Ejercicios;
+
+import java.io.File;
+import java.io.IOException;
+
+public class GuardarArchivos {
+    public static void main(String[] args) {
+        
+    }
+}
