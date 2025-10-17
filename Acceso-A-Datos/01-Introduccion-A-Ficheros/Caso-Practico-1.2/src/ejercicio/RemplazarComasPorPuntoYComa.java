a446bc12406d5d8cebe785ae0fe9bf38e2ae8a3a
GianTroya <giancarlorojastroya@gmail.com>
2025-10-17 21:19:52 +0200

    Modificado el ejercicio 1.2 de Acceso a Datos

--- Acceso-A-Datos/01-Introduccion-A-Ficheros/Caso-Practico-1.2/src/ejercicio/RemplazarComasPorPuntoYComa.java
+++ Acceso-A-Datos/01-Introduccion-A-Ficheros/Caso-Practico-1.2/src/ejercicio/RemplazarComasPorPuntoYComa.java
@@ -7,18 +7,22 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class ReemplazarComasPorPuntoYComa {
+public class RemplazarComasPorPuntoYComa {
 
     public static void main(String[] args) {
-        String textoLargo = "Este es un ejemplo de texto, que contiene varias comas, "
+        // Creamos el texto del fichero
+        String textoLargo =
+                "Este es un ejemplo de texto, que contiene varias comas, "
                 + "para demostrar cómo reemplazarlas, por punto y coma, "
                 + "utilizando Java, de forma sencilla.";
 
+        // Creamos la ruta del fichero si no existe
         File directorio = new File("unidad1_ejemplos");
         if (!directorio.exists()) {
             directorio.mkdirs();
         }
 
+        // Creamos la ruta del archivo original y el modificado en el mismo directorio
         File archivoOriginal = new File(directorio, "texto_original.txt");
         File archivoModificado = new File(directorio, "texto_modificado.txt");
 
@@ -36,7 +40,9 @@ public class ReemplazarComasPorPuntoYComa {
                 BufferedReader reader = new BufferedReader(new FileReader(archivoOriginal));
                 BufferedWriter writer = new BufferedWriter(new FileWriter(archivoModificado))
         ) {
+            // Creamos una variable auxiliar para guardar una línea de texto
             String linea;
+            // Reemplazamos los separadores y los escribimos en el nuevo fichero modificado hasta que no haya ningún texto en la línea
             while ((linea = reader.readLine()) != null) {
                 String lineaModificada = linea.replace(",", ";");
                 writer.write(lineaModificada);
